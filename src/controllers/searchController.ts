32518631bebca3a66b344ac258c52658dc9603d1
evandrocsjr <evandrocunhajunior@gmail.com>
2022-01-26 11:24:34 -0300

    Commit final 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,19 @@
 import { Response, Request } from "express";
+import { createMenuObject } from '../helpers/createMenuObject'
+import { Pet } from "../models/Pet";
 
 export const search = (req: Request, res: Response) => {
-    
+    let query: string = req.query.q as string
+
+    let list = Pet.getFromName(query)
+
+    if(!query){
+        res.redirect('/')
+    }
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 }
